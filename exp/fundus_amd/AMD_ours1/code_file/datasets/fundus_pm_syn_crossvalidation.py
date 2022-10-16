63127088a191194fb7c8c8f24c9f3e3d59016111
afaghiali <afaghiali@yahoo.com>
2022-10-16 08:43:21 +0000

    First Commit

--- exp/fundus_amd/AMD_ours1/code_file/datasets/fundus_pm_syn_crossvalidation.py
+++ exp/fundus_amd/AMD_ours1/code_file/datasets/fundus_pm_syn_crossvalidation.py
@@ -9,7 +9,6 @@ import numpy as np
 from skimage.transform import resize
 from PIL import Image
 
-
 class traindataset(data.Dataset):
     """Face Landmarks dataset."""
 
@@ -25,42 +24,30 @@ class traindataset(data.Dataset):
         self.transform = transform
         self.name = []
         self.train = train
-        self.multitask = args.multitask
         self.multiaug = args.multiaug
+        self.multitask = args.multitask
         self.synthesis = args.synthesis
-        self.domain = args.domain
 
-        images_path = np.genfromtxt(self.root_dir + 'random_list.txt', dtype='str')
+        images_path = np.genfromtxt( self.root_dir + '/PAML/random_list.txt', dtype='str')
         images_path = list(images_path)
-        images_path = [self.root_dir + "/" + item for item in images_path]
-        images_path = [item for item in images_path if item.split("/")[-1] != "A0012.jpg"]
+        images_path = [self.root_dir + item for item in images_path]
 
-        # in each fold, train and test path are fixed.
         num_fold = int(len(images_path) / 5)
         if args.seed == 0:
             test_path = images_path[:num_fold]
-            train_path = images_path[num_fold:]
         elif args.seed == 1:
-            test_path = images_path[num_fold: 2 * num_fold]
-            train_path = images_path[:num_fold]
-            train_path += images_path[2 * num_fold:]
+            test_path = images_path[num_fold:2*num_fold]
         elif args.seed == 2:
             test_path = images_path[2 * num_fold:3 * num_fold]
-            train_path = images_path[: 2 * num_fold]
-            train_path += images_path[3 * num_fold:]
         elif args.seed == 3:
             test_path = images_path[3 * num_fold:4 * num_fold]
-            train_path = images_path[: 3 * num_fold]
-            train_path += images_path[4 * num_fold:]
         elif args.seed == 4:
             test_path = images_path[4 * num_fold:5 * num_fold]
-            train_path = images_path[: 4 * num_fold]
-            train_path += images_path[5 * num_fold:]
 
-        label_list_train = [1 if item.split("/")[-1][0] == "A" else 0 for item in train_path]
-        label_list_test = [1 if item.split("/")[-1][0] == "A" else 0 for item in test_path]
-        print ("train p: ", sum(label_list_train), len(label_list_train)-sum(label_list_train))
-        print ("test p: ", sum(label_list_test), len(label_list_test) - sum(label_list_test))
+        train_path = list(set(images_path) - set(test_path))
+
+        label_list_train = [1 if item.split("/")[-1][0] == "P" else 0 for item in train_path]
+        label_list_test = [1 if item.split("/")[-1][0] == "P" else 0 for item in test_path]
 
         if self.train:
             self.train_dataset = []
@@ -69,29 +56,29 @@ class traindataset(data.Dataset):
             self.train_syn = []
             self.train_syn_label = []
             self.train_syn_name = []
+            image = cv2.imread(args.data + "/PAML/resized_image_320/P0111.jpg")
+            self.train_dataset.append(image)
             for i in range(0, len(train_path)):
-                image = cv2.imread(self.root_dir + "fundus/" + train_path[i].split("/")[-1])
+                image = cv2.imread(args.data + "/PAML/resized_image_320/" + train_path[i].split("/")[-1])
                 self.train_dataset.append(image)
                 self.targets.append(label_list_train[i])
                 self.name.append(train_path[i].split("/")[-1])
                 self.rotation_label.append(0)
                 if self.synthesis:
-                    image = cv2.imread(self.root_dir + "FFA_syn_AMD/" + train_path[i].split("/")[-1][:-4] + ".png")
-                    # image = cv2.imread("../pytorch-CycleGAN-and-pix2pix-master/results/fundusFFA_cyclegan_lr/test_latest/AMD_syn_265/" + train_path[i].split("/")[-1][:-4] + ".png")
+                    image = cv2.imread(self.root_dir + "/PAML/resized_image_syn/" + train_path[i].split("/")[-1][:-4] + ".png")
                     self.train_syn.append(image)
                     self.train_syn_label.append(label_list_train[i])
                     self.train_syn_name.append(train_path[i].split("/")[-1])
-            print("Train images AMD ", len(self.train_dataset), "P: ", sum(self.targets), "N: ", len(self.targets) - sum(self.targets))
+            print("Train images PM ", len(self.train_dataset), "P: ", sum(self.targets), "Neg: ", len(self.targets) - sum(self.targets))
         else:
             self.train_dataset = []
             self.targets = []
             for i in range(0, len(test_path)):
-                image = cv2.imread(self.root_dir + "fundus/" + test_path[i].split("/")[-1])
+                image = cv2.imread(args.data + "/PAML/resized_image_320/" + test_path[i].split("/")[-1])
                 self.train_dataset.append(image)
                 self.targets.append(label_list_test[i])
                 self.name.append(test_path[i].split("/")[-1])
-            print("Test images AMD ", len(self.train_dataset), "P: ", sum(self.targets), "N: ", len(self.targets) - sum(self.targets))
-
+            print("Test images PM ", len(self.train_dataset), "P: ", sum(self.targets), "Neg: ", len(self.targets) - sum(self.targets))
 
     def __len__(self):
         return len(self.train_dataset)
@@ -103,19 +90,15 @@ class traindataset(data.Dataset):
         img    = self.transform(sample)
         target = self.targets[idx]
 
-        if self.train and self.synthesis:
+        if self.train and self.multiaug:
             img2 = self.transform(sample)
+
             img3 = self.transform(sample)
+            syn_sample = self.train_syn[idx]
+            syn_sample = Image.fromarray(np.uint8(syn_sample))
+            img_syn = self.transform(syn_sample)
 
-            sample_syn = self.train_syn[idx]
-            sample_syn = Image.fromarray(np.uint8(sample_syn))
-            img_syn = self.transform(sample_syn)
             return [img, img2, img3, img_syn], [target], idx, self.name[idx]
-
-        elif self.train and self.multiaug:
-
-            img2 = self.transform(sample)
-            return [img, img2], [target], idx, self.name[idx]
 
         return img, target, idx, self.name[idx]
 
--- exp/fundus_amd/AMD_ours1/code_file/supervised_fundus.py
+++ exp/fundus_amd/AMD_ours1/code_file/supervised_fundus.py
@@ -12,16 +12,12 @@ import torchvision.transforms as transforms
 
 import models
 import random
-from lib.LinearAverage import LinearAverage
-from lib.BatchAverage import BatchCriterion
-from lib.BatchAverageRot import BatchCriterionRot
-from lib.BatchAverageFour import BatchCriterionFour
+from sklearn.metrics import roc_auc_score, precision_score, f1_score, recall_score
 from lib.utils import AverageMeter
-from test import kNN
 import numpy as np
 
 from lib.utils import save_checkpoint, adjust_learning_rate
-from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
@@ -44,7 +40,7 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--lr', '--learning-rate', default=0.03, type=float,
+parser.add_argument('--lr', '--learning-rate', default=1e-4, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
@@ -61,7 +57,7 @@ parser.add_argument('--low-dim', default=128, type=int,
                     metavar='D', help='feature dimension')
 parser.add_argument('--nce-k', default=4096, type=int,
                     metavar='K', help='number of negative samples for NCE')
-parser.add_argument('--nce-t', default=0.07, type=float, 
+parser.add_argument('--nce-t', default=0.07, type=float,
                     metavar='T', help='temperature parameter for softmax')
 parser.add_argument('--nce-m', default=0.5, type=float,
                     help='momentum for non-parametric updates')
@@ -71,6 +67,7 @@ parser.add_argument('--iter_size', default=1, type=int,
 parser.add_argument('--result', default="", type=str)
 parser.add_argument('--seedstart', default=0, type=int)
 parser.add_argument('--seed', default=0, type=int)
+parser.add_argument('--seedend', default=5, type=int)
 
 
 parser.add_argument("--synthesis", action="store_true")
@@ -80,21 +77,11 @@ parser.add_argument('--multitask', action="store_true")
 parser.add_argument("--multitaskposrot", action="store_true")
 best_prec1 = 0
 
-parser.add_argument("--domain", action="store_true")
-parser.add_argument("--circle", action="store_true")
-parser.add_argument("--saveembed", type=str, default="")
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-def get_learnable_para(model):
-    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
-    return sum([np.prod(p.size()) for p in model_parameters])
-
 def main():
 
     global args, best_prec1
     args = parser.parse_args()
 
-    #  init seed
     my_whole_seed = 222
     random.seed(my_whole_seed)
     np.random.seed(my_whole_seed)
@@ -106,26 +93,25 @@ def main():
     torch.backends.cudnn.benchmark = False
     os.environ['PYTHONHASHSEED'] = str(my_whole_seed)
 
-
-    for kk_time in range(args.seedstart, args.seedstart+1):
+    for kk_time in range(args.seedstart, args.seedend):
         args.seed = kk_time
         args.result = args.result + str(args.seed)
 
         # create model
-        model = models.__dict__[args.arch](low_dim=args.low_dim, multitask=args.multitask , showfeature=args.showfeature, domain=args.domain,args=args)
-        if torch.cuda.is_available():
-          model = torch.nn.DataParallel(model).cuda()
+        from models.resnet_sup import resnet18, resnet50, resnet34
+        model = resnet34()
 
-         
-        print ('Number of learnable params', get_learnable_para(model)/1000000., " M")
+
+        model = torch.nn.DataParallel(model).cuda()
+
 
         # Data loading code
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
 
         aug = transforms.Compose([transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
-                                  transforms.RandomGrayscale(p=0.2),
-                                  transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+                                  # transforms.RandomGrayscale(p=0.2),
+                                  # transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
                                   transforms.RandomHorizontalFlip(),
                                   transforms.ToTensor(),
                                   normalize])
@@ -135,38 +121,31 @@ def main():
         #                           transforms.Lambda(lambda x: gaussian_blur(x)),
         #                           transforms.ToTensor(),
         #                           normalize])
+        # aug = transforms.Compose([transforms.RandomRotation(60),
+        #                           transforms.RandomResizedCrop(224, scale=(0.6, 1.)),
+        #                           transforms.RandomGrayscale(p=0.2),
+        #                           transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+        #                           transforms.RandomHorizontalFlip(),
+        #                           transforms.ToTensor(),
+        #                             normalize])
         aug_test = transforms.Compose([
-                transforms.Resize((224,224)),
+                transforms.Resize(224),
                 transforms.ToTensor(),
                 normalize])
 
-        # load dataset
-        import datasets.fundus_amd_syn_crossvalidation as medicaldata
+        # dataset
+        import datasets.fundus_amd_crossvalidation as medicaldata
         train_dataset = medicaldata.traindataset(root=args.data, transform=aug, train=True, args=args)
         train_loader = torch.utils.data.DataLoader(
-            train_dataset, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=4, drop_last=True if args.multiaug else False,  worker_init_fn=random.seed(my_whole_seed))
+            train_dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=4, drop_last=True if args.multiaug else False,  worker_init_fn=random.seed(my_whole_seed))
+
 
         valid_dataset = medicaldata.traindataset(root=args.data, transform=aug_test, train=False, args=args)
         val_loader = torch.utils.data.DataLoader(
             valid_dataset, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=4,
             worker_init_fn=random.seed(my_whole_seed))
 
-
-        # define lemniscate and loss function (criterion)
-        ndata = train_dataset.__len__()
-
-        lemniscate = LinearAverage(args.low_dim, ndata, args.nce_t, args.nce_m).to(device)
-
-
-        if args.synthesis:
-            print ("running synthesis")
-            criterion = BatchCriterionFour(1, 0.1, args.batch_size, args).to(device)
-        elif args.multiaug:
-            print ("running cvpr")
-            criterion = BatchCriterion(1, 0.1, args.batch_size, args).to(device)
-        else:
-            criterion = nn.CrossEntropyLoss().to(device)
-
+        criterion = nn.CrossEntropyLoss().cuda()
         optimizer = torch.optim.Adam(model.parameters(), args.lr,
                                      weight_decay=args.weight_decay)
 
@@ -175,27 +154,23 @@ def main():
             if os.path.isfile(args.resume):
                 print("=> loading checkpoint '{}'".format(args.resume))
                 checkpoint = torch.load(args.resume)
-                args.start_epoch = checkpoint['epoch']
-                model.load_state_dict(checkpoint['state_dict'])
-                lemniscate = checkpoint['lemniscate']
-                optimizer.load_state_dict(checkpoint['optimizer'])
+                model_dict = model.state_dict()
+
+                pretrained_dict = {k: v for k, v in checkpoint["state_dict"].items() if k in model_dict}
+                pretrained_dict.pop("module.fc.weight")
+                pretrained_dict.pop("module.fc.bias")
+                # pretrained_dict = {k: v for k, v in checkpoint["net"].items() if k in model_dict}
+                # pretrained_dict.pop("module.conv1.weight")
+                # pretrained_dict.pop("module.conv1.bias")
+
+                model_dict.update(pretrained_dict)
+                model.load_state_dict(model_dict)
                 print("=> loaded checkpoint '{}' (epoch {})"
                       .format(args.resume, checkpoint['epoch']))
+
             else:
                 print("=> no checkpoint found at '{}'".format(args.resume))
 
-        if args.evaluate:
-            knn_num = 100
-            auc, acc, precision, recall, f1score = kNN(args, model, lemniscate, train_loader, val_loader, knn_num, args.nce_t, 2)
-            print ("auc", auc)
-            f = open("savedmodels/result.txt", "a+")
-            f.write("auc: %.4f\n" % (auc))
-            f.write("acc: %.4f\n" % (acc))
-            f.write("pre: %.4f\n" % (precision))
-            f.write("recall: %.4f\n" % (recall))
-            f.write("f1score: %.4f\n" % (f1score))
-            f.close()
-            return
 
         # mkdir result folder and tensorboard
         os.makedirs(args.result, exist_ok=True)
@@ -216,18 +191,23 @@ def main():
                 shutil.copy(file, args.result + "/code_file/" + name)
 
         for epoch in range(args.start_epoch, args.epochs):
-            lr = adjust_learning_rate(optimizer, epoch, args, [1000, 2000])
+            lr = adjust_learning_rate(optimizer, epoch, args, [500, 1000, 1500])
             writer.add_scalar("lr", lr, epoch)
 
             # # train for one epoch
-            loss = train(train_loader, model, lemniscate, criterion, optimizer, epoch, writer)
+            loss = train(train_loader, model, criterion, optimizer)
             writer.add_scalar("train_loss", loss, epoch)
 
+            gap_int = 200
+            if (epoch) % gap_int == 0:
+                loss_val, auc, acc, precision, recall, f1score = supervised_evaluation(model, val_loader)
+                writer.add_scalar("test_auc", auc, epoch)
+                writer.add_scalar("test_acc", acc, epoch)
+                writer.add_scalar("test_precision", precision, epoch)
+                writer.add_scalar("test_recall", recall, epoch)
+                writer.add_scalar("test_f1score", f1score, epoch)
 
-            # save checkpoint
-            if epoch % 1000 == 0 or (epoch in [1600, 1800, 2000]):
-                auc, acc, precision, recall, f1score = kNN(args, model, lemniscate, train_loader, val_loader, 100,
-                                                           args.nce_t, 2)
+
                 # save to txt
                 f = open(args.result+"/result.txt","a+")
                 f.write("epoch " + str(epoch) + "\n")
@@ -237,22 +217,63 @@ def main():
                 f.write("recall: %.4f\n" % (recall))
                 f.write("f1score: %.4f\n" % (f1score))
                 f.close()
+
+
+                # save checkpoint
+            if epoch in [1000,2000,3000]:
                 save_checkpoint({
                     'epoch': epoch,
                     'arch': args.arch,
                     'state_dict': model.state_dict(),
-                    'lemniscate': lemniscate,
                     'optimizer' : optimizer.state_dict(),
-                }, filename = args.result + "/fold" +str(args.seedstart)+"-epoch-" +str(epoch) + ".pth.tar")
+                }, filename = args.result + "/epoch-" +str(epoch) + ".pth.tar")
 
 
-def train(train_loader, model, lemniscate, criterion, optimizer, epoch, writer):
+
+def supervised_evaluation(model, val_loader):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+
+    # switch to evaluate mode
+    model.train()
+
+    prediction_box = []
+    target_box = []
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target, index, name) in enumerate(val_loader):
+            images = images.cuda()
+            target = target.cuda()
+            output = model(images)
+
+            output = torch.softmax(output,dim=1)
+            output = output.data.cpu().numpy()
+            output = np.argmax(output, axis=1)
+
+            prediction_box += list(output)
+            target_box += list(target.data.cpu().numpy())
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+
+    auc = roc_auc_score(target_box, prediction_box)
+    corrects = np.equal(np.array(target_box), np.array(prediction_box))
+    acc = float(sum(corrects)) / len(corrects)
+
+    #  mean class
+    precision = precision_score(target_box, prediction_box, average='macro')
+    recall = recall_score(target_box, prediction_box, average='macro')
+    f1score = f1_score(target_box, prediction_box, average='macro')
+
+    return losses.avg, round(auc, 4), round(acc, 4), round(precision, 4),  round(recall, 4),  round(f1score, 4)
+
+
+def train(train_loader, model, criterion, optimizer):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
-
-    losses_ins = AverageMeter()
-    losses_rot = AverageMeter()
 
     # switch to train mode
     model.train()
@@ -264,61 +285,26 @@ def train(train_loader, model, lemniscate, criterion, optimizer, epoch, writer):
         # measure data loading time
         data_time.update(time.time() - end)
 
-        # compute output
-        if args.synthesis:
-            dataX = torch.cat(input,0).to(device)
-            ori_data = dataX[:int(dataX.shape[0]/2)]
-            syn_data = dataX[int(dataX.shape[0]/2):]
-            data = [ori_data, syn_data]
-            dataX = torch.stack(data, dim=1).to(device)
-            batch_size, types, channels, height, width = dataX.size()
-            input = dataX.view([batch_size * types, channels, height, width])
+        input = input.cuda()
+        target = target.cuda()
 
-            # instance discrimination
-            # input = torch.cat(input, 0).cuda()
-            feature = model(input)
-            loss = criterion(feature, index) / args.iter_size
-        elif args.multiaug:
 
-            input = torch.cat(input, 0).to(device)
-            feature = model(input)
-            loss = criterion(feature, index) / args.iter_size
-        else:
-            # instance discrimination memory bank
-            input = input.cuda()
-            index = index.cuda()
+        output = model(input)
 
-            feature = model(input)
-            output = lemniscate(feature, index)
-            loss = criterion(output, index) / args.iter_size
+        loss = criterion(output, target)
 
+        optimizer.zero_grad()
         loss.backward()
+        optimizer.step()
 
         # measure accuracy and record loss
         losses.update(loss.item() * args.iter_size, input.size(0))
-
-        if (i+1) % args.iter_size == 0:
-            # compute gradient and do SGD step
-            optimizer.step()
-            optimizer.zero_grad()
 
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
-                   epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses))
-
-    writer.add_scalar("losses_ins", losses_ins.avg, epoch)
-    writer.add_scalar("losses_rot", losses_rot.avg, epoch)
-
     return losses.avg
-
 
 
 if __name__ == '__main__':
